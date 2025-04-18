14823fd53a59a1939eb88cb84074b0905d2722d8
tusharsheikh1 <tushar.mkt15@gmail.com>
2025-04-19 02:04:19 +0600

    created .env file in frontend

--- mc/src/utils/api.jsx
+++ mc/src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust according to your backend URL
+  baseURL: process.env.REACT_APP_API_URL, // Dynamic backend URL from .env
 });
 
 // Add a request interceptor to include the token in headers
@@ -23,7 +23,6 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      // Handle unauthorized errors (e.g., redirect to login)
       console.error('Unauthorized! Redirecting to login...');
       window.location.href = '/login';
     }
